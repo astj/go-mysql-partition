3a1ec7b90c5a06df05ce50a9a78ab2bd51083644
Asato Wakisaka <asato.wakisaka@gmail.com>
2024-02-20 01:00:39 +0900

    example Output needs output

--- partition_example_test.go
+++ partition_example_test.go
@@ -2,6 +2,7 @@ package partition_test
 
 import (
 	"database/sql"
+	"fmt"
 
 	partition "github.com/astj/go-mysql-partition"
 )
@@ -9,13 +10,15 @@ import (
 func ExampleType_listColumns() {
 	var db *sql.DB
 	list := partition.NewListPartitioner(db, "tableName", "columnName", partition.Type("LIST COLUMNS"))
-	list.PrepareCreates(partition.NewPartition("p_01", "'v1', 'v2', 'v3'", "comment"))
+	handler, _ := list.PrepareCreates(partition.NewPartition("p_01", "'v1', 'v2', 'v3'", "comment"))
+	fmt.Println(handler.Statement())
 	// Output: ALTER TABLE tableName PARTITION BY LIST COLUMNS (columnName) (PARTITION p_01 VALUES IN ('v1', 'v2', 'v3') COMMENT = 'comment')
 }
 
 func ExampleType_rangeColumns() {
 	var db *sql.DB
 	list := partition.NewRangePartitioner(db, "tableName", "created_at", partition.Type("RANGE COLUMNS"))
-	list.PrepareCreates(partition.NewPartition("p_01", "2010-01-01", "comment"))
-	// Output: ALTER TABLE tableName PARTITION BY RANGE COLUMNS (created_at) (PARTITION p20100101 VALUES LESS THAN ('2010-01-01'))
+	handler, _ := list.PrepareCreates(partition.NewPartition("p_01", "2010-01-01", "comment"))
+	fmt.Println(handler.Statement())
+	// Output: ALTER TABLE tableName PARTITION BY RANGE COLUMNS (created_at) (PARTITION p_01 VALUES LESS THAN ('2010-01-01') COMMENT = 'comment')
 }
